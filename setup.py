65ed4248155f45de45a2e77a35cdecade30420d3
Mike Kwiatkowsky <mikeski885@gmail.com>
2026-01-06 16:38:24 -0500

    moved setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mkl",
+    name="mk",
     version="0.1.0",
     description="Mike Grocery List CLI App",
     author="Mike Kwiatkowsky",
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(where="src"),
     entry_points={
         "console_scripts": [
-            "mkl=mkl.mk_launch:main",
+            "mk=mk.mk_launch:main",
         ],
     },
     install_requires=[],
